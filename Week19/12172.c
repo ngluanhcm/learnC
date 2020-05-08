c2b184c3519ad8e2e2685b1f61d0635e5256632e
Luan_Ubuntu <ngluanhcmus@gmail.com>
2020-05-08 10:28:58 +0700

    update

---
+++ Week19/12172.c
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+
+	main()
+
+	{ int i;
+
+	   printf("\t Number \t\t Square Root of Number\n\n");
+
+	   for (i=0; i<=360; ++i)
+            printf("\t %d \t\t\t %f \n",i, sqrt((double) i));
+
+	}
