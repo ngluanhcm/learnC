2fc0a272e3214c1f8b54a057ed74533256cc7cad
LuanNguyen <ngluanhcmus@gmail.com>
2020-05-14 12:48:46 +0000

    update

---
+++ Week19/12332.c
@@ -0,0 +1,21 @@
+/*
+Exercise 12332
+Write a program to count the vowels and letters in free text given as standard input. Read text a
+character at a time until you encounter end-of-data.
+Then print out the number of occurrences of each of the vowels a, e, i, o and u in the text, the total
+number of letters, and each of the vowels as an integer percentage of the letter total.
+Suggested output format is:
+Numbers of characters:
+a 3 ; e 2 ; i 0 ; o 1 ; u 0 ; rest 17
+Percentages of total:
+a 13%; e 8%; i 0%; o 4%; u 0%; rest 73%
+Read characters to end of data using a construct such as
+char ch;
+while(
+( ch = getchar() ) >= 0
+) {
+//ch is the next character  ....
+}
+to read characters one at a time using getchar() until a negative value is returned.
+*/
+
